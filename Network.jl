4ea10fd9231208469a8b597c98f378896922b26a
= <=>
2019-06-09 23:16:02 +0200

    Implemented feedforward.

--- Network.jl
+++ Network.jl
@@ -3,11 +3,20 @@ struct Network
     weights::Array{Array{Float64,2},1}
 end
 
-function Network(layers::Array{Int64,1})
-    Network([randn(x) for x in layers[2:end]], [randn(x, y) for x in layers[2:end] for y in layers[1:end-1]])
+function Network(layers::Array{Int,1})::Network
+    Network([randn(x) for x in layers[2:end]], [randn(x, y) for (x, y) in zip(layers[2:end], layers[1:end-1])])
 end
 
-network = Network([16, 20, 10])
+function feedforward(network::Network, input::Array{Float64,1})::Array{Float64,1}
+    a = input
+    for (b, w) in zip(network.biases, network.weights)
+        a = sigmoid.(w*a .+ b)
+    end
+    a
+end
 
-println(size(network.biases))
-println(size(network.weights))
+sigmoid(y) = 1 / (1 + exp(-y))
+
+network = Network([2, 5, 2])
+
+println(feedforward(network, [1., 1.]))
